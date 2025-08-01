069d284f2ebebcd4aa010b6c56cc31873e47745f
zai <pristinevvs@gmail.com>
2025-08-01 17:43:51 -0400

    Commit 2 - added comments

---
+++ A4 - JS/A4P2 - Image gallery/gallery-start/gallery-start/main.js
@@ -0,0 +1,26 @@
+/*
+   Name: Rustam Zazai
+   File: main.js
+   Date: August 1 2025
+   This is the JavaScript for the image gallery part
+   (INFT1206-06)
+   */
+
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+/* Declaring the alternative text for each image file */
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
---
+++ A4 - JS/A4P3 - Object building practice/main.js
@@ -0,0 +1,27 @@
+/*
+   Name: Rustam Zazai
+   File: main.js
+   Date: August 1 2025
+   This is the JavaScript for the object building practice
+   (INFT1206-06)
+   */
+
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
